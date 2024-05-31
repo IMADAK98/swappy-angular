212c3d264ef6ffa58e462442cd68399d811cb9f5
IMADAK98 <imadka224@gmail.com>
2024-05-31 15:55:31 +0300

    update to angular 18

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,11 +5,7 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import {
-  provideHttpClient,
-  withFetch,
-  withInterceptors,
-} from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { authInterceptor } from './auth/auth.interceptor';
 import { AppErrorHandler } from './errors/app-error-handler';
